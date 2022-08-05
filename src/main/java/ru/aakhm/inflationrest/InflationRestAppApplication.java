cb1858e4cb46e96964c4eb6372b130aadcb682db
Arcanum <arkgah@gmail.com>
2022-08-05 17:06:00 +0300

    Initial commit
    Начальный шаблон проекта из Spring initializr

---
+++ src/main/java/ru/aakhm/inflationrest/InflationRestAppApplication.java
@@ -0,0 +1,13 @@
+package ru.aakhm.inflationrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflationRestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InflationRestAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/aakhm/inflationrest/InflationRestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.aakhm.inflationrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflationRestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
