5a50e26d5dd94b536a5992f9129c3a36bf829e9e
Arcanum <arkgah@gmail.com>
2022-08-18 17:41:41 +0300

    Add ProductsServiceTest.update()
    + рефакторинг кода

--- src/test/java/ru/aakhm/inflationrest/services/ProductsServiceTest.java
+++ src/test/java/ru/aakhm/inflationrest/services/ProductsServiceTest.java
@@ -12,14 +12,16 @@ import ru.aakhm.inflationrest.dto.out.ProductCategoryOutDTO;
 import ru.aakhm.inflationrest.dto.out.ProductOutDTO;
 import ru.aakhm.inflationrest.models.Product;
 import ru.aakhm.inflationrest.models.ProductCategory;
+import ru.aakhm.inflationrest.models.validation.except.product.ProductNameCategoryUniqException;
+import ru.aakhm.inflationrest.models.validation.except.product.ProductNotFoundException;
+import ru.aakhm.inflationrest.models.validation.except.productcategory.ProductCategoryNotFoundException;
 import ru.aakhm.inflationrest.repo.ProductCategoriesRepo;
 import ru.aakhm.inflationrest.repo.ProductsRepo;
 import ru.aakhm.inflationrest.utils.Utils;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -40,71 +42,86 @@ class ProductsServiceTest {
     private ProductCategoriesRepo productCategoriesRepo;
 
 
+    private ProductInDTO productInDTO;
+    private Product product;
+    private ProductCategory productCategory;
+    private ProductOutDTO productOutDTO;
+
+    private final String PRODUCT_EXTERNAL_ID = "123abc";
+    private final String PRODUCT_NAME = "Test Product";
+    private final String PRODUCT_CAT_NAME = "Test Category";
+    private final String PRODUCT_CAT_EXTERNAL_ID = "456def";
+    private final Double UNIT = 0.5;
+
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
+        initProducts();
     }
 
-    @Test
-    void save() {
-        final String EXTERNAL_ID = "123abc";
-        final String PRODUCT_NAME = "Test Product";
-        final String PRODUCT_CAT_NAME = "Test Category";
-        final double UNIT = 0.5;
-
-        ProductInDTO pInDTO = new ProductInDTO() {
+    private void initProducts() {
+        productInDTO = new ProductInDTO() {
             {
                 setName(PRODUCT_NAME);
                 setUnit(UNIT);
                 setCategory(new ProductCategoryInDTO() {
                     {
-                        setName(PRODUCT_NAME);
-                    }
-                });
-            }
-        };
-
-        Product product = new Product() {
-            {
-                setName(PRODUCT_NAME);
-                setExternalId(EXTERNAL_ID);
-            }
-        };
-
-        ProductCategory productCategory = new ProductCategory() {
-            {
-                setName(PRODUCT_CAT_NAME);
-            }
-        };
-
-        ProductOutDTO pOutDTO = new ProductOutDTO() {
-            {
-                setName(PRODUCT_NAME);
-                setCategory(new ProductCategoryOutDTO() {
-                    {
                         setName(PRODUCT_CAT_NAME);
                     }
                 });
-                setExternalId(EXTERNAL_ID);
             }
         };
 
+        productCategory = new ProductCategory() {
+            {
+                setName(PRODUCT_CAT_NAME);
+                setExternalId(PRODUCT_CAT_EXTERNAL_ID);
+            }
+        };
+
+        product = new Product() {
+            {
+                setId(1);
+                setName(PRODUCT_NAME);
+                setUnit(UNIT);
+                setExternalId(PRODUCT_EXTERNAL_ID);
+                setCategory(productCategory);
+            }
+        };
+
+        productOutDTO = new ProductOutDTO() {
+            {
+                setName(PRODUCT_NAME);
+                setUnit(UNIT);
+                setCategory(new ProductCategoryOutDTO() {
+                    {
+                        setName(PRODUCT_CAT_NAME);
+                        setExternalId(PRODUCT_CAT_EXTERNAL_ID);
+                    }
+                });
+                setExternalId(PRODUCT_EXTERNAL_ID);
+            }
+        };
+    }
+
+    @Test
+    void save() {
         when(productsRepo.save(any(Product.class))).thenReturn(product);
-        when(utils.generateExternalId()).thenReturn(EXTERNAL_ID);
+        when(utils.generateExternalId()).thenReturn(PRODUCT_EXTERNAL_ID);
 
         when(productCategoriesRepo.getByName(any(String.class))).thenReturn(Optional.of(productCategory));
 
         when(modelMapper.map(any(ProductInDTO.class), any())).thenReturn(product);
-        when(modelMapper.map(any(Product.class), any())).thenReturn(pOutDTO);
+        when(modelMapper.map(any(Product.class), any())).thenReturn(productOutDTO);
 
-        ProductOutDTO resProduct = productsService.save(pInDTO);
+        ProductOutDTO resProduct = productsService.save(productInDTO);
         verify(productsRepo, times(1)).save(any(Product.class));
         verify(modelMapper, times(1)).map(any(ProductInDTO.class), any());
         verify(modelMapper, times(1)).map(any(Product.class), any());
 
         assertNotNull(resProduct);
         assertEquals(PRODUCT_NAME, resProduct.getName());
-        assertEquals(EXTERNAL_ID, resProduct.getExternalId());
+        assertEquals(PRODUCT_EXTERNAL_ID, resProduct.getExternalId());
 
         assertNotNull(resProduct.getCategory());
         assertEquals(PRODUCT_CAT_NAME, resProduct.getCategory().getName());
@@ -112,6 +129,41 @@ class ProductsServiceTest {
 
     @Test
     void update() {
+        // product && productCategory существуют, имя товара и категория уникальны
+        when(productsRepo.getByExternalId(PRODUCT_EXTERNAL_ID)).thenReturn(Optional.of(product));
+        when(productsRepo.save(any(Product.class))).thenReturn(product);
+        when(productsRepo.getProductByNameAndCategory(PRODUCT_NAME, productCategory)).thenReturn(Optional.empty());
+
+        when(productCategoriesRepo.getByName(PRODUCT_CAT_NAME)).thenReturn(Optional.of(productCategory));
+        when(modelMapper.map(any(ProductInDTO.class), any())).thenReturn(product);
+        when(modelMapper.map(any(Product.class), any())).thenReturn(productOutDTO);
+
+        ProductOutDTO resProduct = productsService.update(PRODUCT_EXTERNAL_ID, productInDTO);
+        verify(productsRepo, times(1)).getProductByNameAndCategory(PRODUCT_NAME, productCategory);
+        verify(productsRepo, times(1)).save(any(Product.class));
+        assertNotNull(resProduct);
+        assertEquals(PRODUCT_EXTERNAL_ID, resProduct.getExternalId());
+        assertEquals(PRODUCT_NAME, resProduct.getName());
+        assertEquals(UNIT, resProduct.getUnit());
+        assertNotNull(resProduct.getCategory());
+        assertEquals(PRODUCT_CAT_EXTERNAL_ID, resProduct.getCategory().getExternalId());
+        assertEquals(PRODUCT_CAT_NAME, resProduct.getCategory().getName());
+
+        // product не существует
+        when(productsRepo.getByExternalId(PRODUCT_EXTERNAL_ID)).thenReturn(Optional.empty());
+        assertThrows(ProductNotFoundException.class, () -> productsService.update(PRODUCT_EXTERNAL_ID, productInDTO));
+
+        // productCategory не существует
+        when(productsRepo.getByExternalId(PRODUCT_EXTERNAL_ID)).thenReturn(Optional.of(product));
+        when(productCategoriesRepo.getByName(PRODUCT_CAT_NAME)).thenReturn(Optional.empty());
+        assertThrows(ProductCategoryNotFoundException.class, () -> productsService.update(PRODUCT_EXTERNAL_ID, productInDTO));
+
+        // неуникальная имя и категория товара
+        when(productsRepo.getProductByNameAndCategory(PRODUCT_NAME, productCategory)).thenReturn(Optional.of(product));
+        when(productsRepo.getByExternalId(PRODUCT_EXTERNAL_ID)).thenReturn(Optional.of(product));
+        when(productCategoriesRepo.getByName(PRODUCT_CAT_NAME)).thenReturn(Optional.of(productCategory));
+        assertThrows(ProductNameCategoryUniqException.class, () -> productsService.update(PRODUCT_EXTERNAL_ID, productInDTO));
+
     }
 
     @Test
